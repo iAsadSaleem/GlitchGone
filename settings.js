22e8d880b29eb64fe11e32f6d94640672d61f43f
HaseebHarry <haseebharry07@gmail.com>
2025-09-21 02:26:01 +0500

    asdf

--- settings.js
+++ settings.js
@@ -294,7 +294,8 @@
         "--second-color": "Choose Secondary Color",  // ✅ replaced header-bg-color
         "--sidebar-bg-color": "Choose Sidebar BG Color",
         "--sidebar-menu-bg": "Choose Sidebar Menu BG Color",
-        "--sidebar-menu-color": "Choose SideBar Text Color"
+        "--sidebar-menu-color": "Choose SideBar Text Color",
+        "--sidebar-menu-icon-active-color": "Choose SideBar Icon Color",
     };
 
     function createColorPicker(labelText, storageKey, cssVar, applyFn) {
@@ -605,6 +606,7 @@
             "--sidebar-bg-color",
             "--sidebar-menu-bg",
             "--sidebar-menu-color",
+            "--sidebar-menu-icon-color",
         ];
 
         function updateSidebarGradient() {
