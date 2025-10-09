6b0b36b741bcea5ed7d860ec1138b4735b3b1d48
HaseebHarry <haseebharry07@gmail.com>
2025-10-09 23:57:02 +0500

    asdf

--- settings.js
+++ settings.js
@@ -183,7 +183,7 @@
                 successOverlay.style.display = "none";
                 yesBtn.disabled = false; // Enable again if needed
                 onYes && onYes();
-            }, 900);
+            }, 800);
         });
 
         modal.querySelector("#jc-no-btn").addEventListener("click", () => {
