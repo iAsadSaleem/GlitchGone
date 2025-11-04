1bc66cfcfe4960d5773731dd18899b26ed579ec2
HaseebHarry <haseebharry07@gmail.com>
2025-11-05 00:47:22 +0500

    asdf

--- settings.js
+++ settings.js
@@ -503,6 +503,7 @@
         if (!container) return;
         const savedThemeObj = JSON.parse(localStorage.getItem("userTheme") || "{}");
         const selectedthemename = localStorage.getItem("themebuilder_selectedTheme");
+        const selectedtheme = '';
         // inject minimal styles once
         if (!document.getElementById("tb-theme-selector-styles")) {
             const s = document.createElement("style");
