508d73e428ee317aab1625e7fbad42a7c4eeb0ee
HaseebHarry <haseebharry07@gmail.com>
2025-11-20 23:13:46 +0500

    updated code

--- settings.js
+++ settings.js
@@ -5451,12 +5451,11 @@
                     document.body.classList.toggle("dark-mode", isDark);
                 });
              //Get saved theme and mode
-            const abc = JSON.parse(localStorage.getItem("userTheme") || "{}");
-            console.log(abc, 'Here is First Usertheem data');
-            const savedThemeObj = JSON.parse(localStorage.getItem("userTheme") || "{}");
-            console.log(savedThemeObj, 'here is the savedThemeObj theme data');
-            const selectedTheme = localStorage.getItem("themebuilder_selectedTheme");
-            const currentMode = savedThemeObj?.themeData?.["--theme-mode"];
+            //const abc = JSON.parse(localStorage.getItem("userTheme") || "{}");
+            //console.log(abc, 'Here is First Usertheem data');
+            //const savedThemeObj = JSON.parse(localStorage.getItem("userTheme") || "{}");
+            //const selectedTheme = localStorage.getItem("themebuilder_selectedTheme");
+            //const currentMode = savedThemeObj?.themeData?.["--theme-mode"];
 
             // Your dark -> light mapping
             const darkThemes = darkthemes();
