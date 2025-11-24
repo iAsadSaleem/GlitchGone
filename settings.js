6272b8be3535a3c440188c265bce885bbcf64c2a
HaseebHarry <haseebharry07@gmail.com>
2025-11-24 21:53:34 +0500

    updated code

--- settings.js
+++ settings.js
@@ -1746,7 +1746,7 @@
             // ----------------------------------------------
             const isSubAccount = window.location.pathname.startsWith("/v2/location/");
 
-            if (themeName === "BlueWave Theme" && isSubAccount) {
+            if (isSubAccount) {
                 window.__BLUEWAVE_TOPNAV_ENABLED__ = true;
                 enableBlueWaveTopNav();
             } else {
