5c464c01f3f095d75483ea5115f1fd4c56ce7531
HaseebHarry <haseebharry07@gmail.com>
2026-01-03 00:21:34 +0500

    themes updated

--- settings.js
+++ settings.js
@@ -806,13 +806,12 @@
                 "--lockedMenus": "{}",
                 "--body-font": "Roboto",
                 "--login-company-logo": "url('https://msgsndr-private.storage.googleapis.com/companyPhotos/47b7e157-d197-4ce5-9a94-b697c258702a.png')",
-                "--login-background-active": "linear-gradient(135deg, #d6d8ff 0%, #b7afff 100%)",
+                "--login-background-active": "linear-gradient(135deg, #b7afff 0%, #b7afff 100%)",
                 "--login-background-image": "url('https://theme-builder-delta.vercel.app/images/velvetbgimg.png')",
                 "--loader-background-color": "linear-gradient(to bottom, #17153B, #2E236C 60%, #433D8B 100%)",
                 "--loader-color-rgb": "255, 255, 255",
                 "--go-back-text-color": "#ffffff",
                 "--go-back-color": "#000000",
-                "--login-background-active": "linear-gradient(90deg, #ffe1e1 60%, #ffe6e6 100%)",
                 "--login-headline-text-color": "#00c853",
                 "--login-button-text-color":"#ffffff"
 
