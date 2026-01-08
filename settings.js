d7d1a249190797bdf581b8aba2a5cece65df07cf
HaseebHarry <haseebharry07@gmail.com>
2026-01-09 03:34:39 +0500

    updated code

--- settings.js
+++ settings.js
@@ -4857,7 +4857,7 @@ function addCursorPointerSelectorSettings(container) {
         wrapper.appendChild(mainTitle);
 
         const subAccountContainer = document.createElement("div");
-        subAccountMenus.forEach(menu => createToggleRow(menu, lockedMenus, hiddenMenus, subAccountContainer));
+        sidebarMenus.forEach(menu => createToggleRow(menu, lockedMenus, hiddenMenus, subAccountContainer));
         wrapper.appendChild(subAccountContainer);
 
         mainTitle.addEventListener("click", () => {
