b90182ce938655c3d54a38c991954c946c1b9c7e
HaseebHarry <haseebharry07@gmail.com>
2026-04-02 03:54:30 +0500

    updated code for test commented reload location

--- settings.js
+++ settings.js
@@ -6320,7 +6320,7 @@
                                         console.error("Error sending loader-css status:", error);
                                     }
 
-                                    location.reload();
+                                    // location.reload();
                                 } catch (error) {
                                     console.error(error);
                                     loaderOverlay.style.display = "none";
