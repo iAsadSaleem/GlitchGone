72caa084c342a431032c05b48d369cf131b4d21f
HaseebHarry <haseebharry07@gmail.com>
2026-04-03 23:19:58 +0500

    updated code agency hide menu is working till ehre

--- settings.js
+++ settings.js
@@ -6340,7 +6340,7 @@ function cleanupMenuStates() {
                                         console.error("Error sending loader-css status:", error);
                                     }
 
-                                    // location.reload();
+                                    location.reload();
                                 } catch (error) {
                                     console.error(error);
                                     loaderOverlay.style.display = "none";
